5016ff5f7b5b943624849466f2e955ae5313b7e6
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-05-16 01:01:06 +0900

    🎉 init

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,48 @@
+import {
+	useVideoConfig,
+	useCurrentFrame,
+	spring,
+	Img,
+	staticFile,
+} from 'remotion';
 export const MyComposition = () => {
-	return null;
+	const frame = useCurrentFrame();
+	const {fps} = useVideoConfig();
+
+	return (
+		<div
+			style={{
+				width: '100%',
+				height: '100%',
+				backgroundColor: '#f2511b',
+				color: '#fff',
+				textAlign: 'center',
+				alignItems: 'center',
+				justifyContent: 'center',
+				fontSize: '5rem',
+				fontFamily: 'Arial',
+			}}
+		>
+			<h1
+				style={{
+					display: 'inline-block',
+					padding: '10px',
+					border: `${frame / 20}px solid white`,
+					borderRadius: `${frame / 5}px`,
+					boxShadow: `11px 10px ${frame * 2}px 0px rgba(0,0,0,0.75)`,
+					transform: `scale(${spring({fps, frame})})`,
+				}}
+			>
+				Pandashark
+				<Img
+					src={staticFile('pandashark0.svg')}
+					style={{
+						width: '40px',
+						height: '40px',
+						transform: `rotate(${frame * 15}deg)`,
+					}}
+				/>
+			</h1>
+		</div>
+	);
 };
